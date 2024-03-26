edafb3a9c07d017fffb11693c62f6df90182c4e8
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-03-26 13:12:32 +0200

    finish

--- src/components/mainTitle/MainTitle.jsx
+++ src/components/mainTitle/MainTitle.jsx
@@ -1,22 +1,5 @@
-import { BtnList } from "../btn/BtnList";
-import { Description } from "../description/Description";
-import { MainTitle } from "../mainTitle/MainTitle";
-import style from './Main.module.css';
-import mainImg from '../../assets/img/astronautas.png';
+import style from './MainTitle.module.css';
 
-function Main() {
-    return (
-        <main className="container">
-            <div className="column-left">
-                <MainTitle />
-                <Description />
-                <BtnList />
-            </div>
-            <div className="column-right">
-                <img className={style.mainImg} src={mainImg} alt="Astronautas raso zinute namo, nes isskrydo i kosmosa" />
-            </div>
-        </main>
-    );
+export function MainTitle() {
+    return <h1 className={style.mainTitle}><span>404</span>Lost in space</h1>;
 }
-
-export { Main };
